79b2cf46051baf5680b9b0ceda2afd3e85d739d8
Nithiwat Seesillapachai <ns271@duke.edu>
2020-05-26 15:53:54 +0700

    aws

--- api/server/aws.js
+++ api/server/aws.js
@@ -1,1 +1,10 @@
-const aws = require("aws-sdk");
+const AWS = require("aws-sdk");
+
+// config my AWS instance
+const config = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_BUCKET_REGION,
+};
+
+AWS.config.update(config);
